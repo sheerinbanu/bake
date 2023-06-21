1bc5ba9de44bb3c5246f6338d06f78a84c5c5659
SheerinBanu <sheemuzz81@gmail.com>
2023-06-21 14:06:55 +0200

    Project structure

---
+++ src/main/java/com/ecommerce/bake/BakeApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.bake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/bake/BakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.bake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
